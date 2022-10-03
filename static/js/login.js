363a20621990840394ceaac0adbebbb45ce1aafa
Tomi <tomicaceres07@gmail.com>
2022-10-03 12:13:05 -0300

    post to admin

--- static/js/login.js
+++ static/js/login.js
@@ -19,6 +19,21 @@ window.addEventListener('load', () => {
             document.getElementById("pass-required").classList.add("show")
         }
 
+        let _datos = {
+            email: email,
+            password: pass,
+            action: "login"
+        }
+        
+        console.log(_datos);
 
+        fetch('http://192.168.2.252:5000/admin', {
+            method: "POST",
+            body: JSON.stringify(_datos),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        .then(response => response.json()) 
+        .then(json => console.log(json))
+        .catch(err => console.log(err));
     })
 })
