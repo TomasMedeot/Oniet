49c9bf2cdfb4b5df5ff6fb8b81ec578ccfea6926
Tomi <tomicaceres07@gmail.com>
2022-10-03 12:09:01 -0300

    Email and pass required

--- static/js/login.js
+++ static/js/login.js
@@ -1,8 +1,24 @@
 window.addEventListener('load', () => {
     const buttonSubmit = document.getElementById("login__button-submit");
-    
+
+
     buttonSubmit.addEventListener('click', (e) => {
         e.preventDefault();
+        const email = document.getElementById("login__email-input").value
+        const pass = document.getElementById("login__pass-input").value
         
+        if (email !== '') {
+            document.getElementById("email-required").classList.remove("show")
+        } else {
+            document.getElementById("email-required").classList.add("show")
+        }
+
+        if (pass !== '') {
+            document.getElementById("pass-required").classList.remove("show")
+        } else {
+            document.getElementById("pass-required").classList.add("show")
+        }
+
+
     })
 })
